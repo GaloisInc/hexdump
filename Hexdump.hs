1c9af38e056f88210e8b19ffbb37a529806e0abf
Alexandr Makoed <me@dsfn.gq>
2018-08-14 17:38:36 +0300

    Address width increased, column pointers and borders added

--- Hexdump.hs
+++ Hexdump.hs
@@ -1,11 +1,11 @@
 module Hexdump (prettyHex, simpleHex) where
 
-import Data.ByteString                       (ByteString)
-import qualified Data.ByteString       as B  (length, unpack)
+import           Data.ByteString       (ByteString)
+import qualified Data.ByteString       as B (length, unpack)
 import qualified Data.ByteString.Char8 as B8 (unpack)
-import Data.Char                             (isAscii, isControl)
-import Data.List                             (intercalate, transpose, unfoldr)
-import Numeric                               (showHex)
+import           Data.Char             (isAscii, isControl)
+import           Data.List             (intercalate, transpose, unfoldr)
+import           Numeric               (showHex)
 
 byteWidth    = 2  -- Width of an padded 'Word8'
 numWordBytes = 4  -- Number of bytes to group into a 32-bit word
@@ -15,49 +15,94 @@ numWordBytes = 4  -- Number of bytes to group into a 32-bit word
 --
 -- Sample output
 --
--- @Length: 100 (0x64) bytes
---0000:   4b c1 ad 8a  5b 47 d7 57  48 64 e7 cc  5e b5 2f 6e   K...[G.WHd..^./n
---0010:   c5 b3 a4 73  44 3b 97 53  99 2d 54 e7  1b 2f 91 12   ...sD;.S.-T../..
---0020:   c8 1a ff c4  3b 2b 72 ea  97 e2 9f e2  93 ad 23 79   ....;+r.......#y
---0030:   e8 0f 08 54  02 14 fa 09  f0 2d 34 c9  08 6b e1 64   ...T.....-4..k.d
---0040:   d1 c5 98 7e  d6 a1 98 e2  97 da 46 68  4e 60 11 15   ...~......FhN`..
---0050:   d8 32 c6 0b  70 f5 2e 76  7f 8d f2 3b  ed de 90 c6   .2..p..v...;....
---0060:   93 12 9c e1                                          ....@
+--            +----------------------------------------------------+
+--            |  0  1  2  3  4   5  6  7  8  9  a  b   c  d  e  f  |
+-- +----------+----------------------------------------------------+------------------+
+-- | 00000000 | 4b c1 ad 8a  5b 47 d7 57  48 64 e7 cc  5e b5 2f 6e | K...[G.WHd..^./n |
+-- | 00000010 | c5 b3 a4 73  44 3b 97 53  99 2d 54 e7  1b 2f 91 12 | ...sD;.S.-T../.. |
+-- | 00000020 | c8 1a ff c4  3b 2b 72 ea  97 e2 9f e2  93 ad 23 79 | ....;+r.......#y |
+-- | 00000030 | e8 0f 08 54  02 14 fa 09  f0 2d 34 c9  08 6b e1 64 | ...T.....-4..k.d |
+-- | 00000040 | d1 c5 98 7e  d6 a1 98 e2  97 da 46 68  4e 60 11 15 | ...~......FhN`.. |
+-- | 00000050 | d8 32 c6 0b  70 f5 2e 76  7f 8d f2 3b  ed de 90 c6 | .2..p..v...;.... |
+-- | 00000060 | 93 12 9c e1                                        | ....@            |
+-- +----------+----------------------------------------------------+------------------+
+--  @Length: 100 (0x64) bytes
+
+-- Calculated width of the hex display panel
+hexDisplayWidth :: Int
+hexDisplayWidth = 50
+
+-- Number of words to group onto a line
+numLineWords :: Int
+numLineWords = 4
+
+-- Minimum width of a padded address
+addressWidth :: Int
+addressWidth = 8
+
+-- Number of bytes on a line
+numLineBytes :: Int
+numLineBytes = numLineWords * numWordBytes
+
+-- 'Char' to use for non-printable characters
+replacementChar :: Char
+replacementChar = '.'
+
+border :: String
+border =
+  concat [
+    '+' : replicate (addressWidth + 2) '-'
+    , '+' : replicate (hexDisplayWidth + 2) '-'
+    , '+' : replicate (numLineBytes + 2) '-'
+    , ['+']
+  ]
+
+header :: [String]
+header =
+  [ concat [
+      replicate (addressWidth + 3) ' '
+      , '+' : replicate (hexDisplayWidth + 2) '-'
+      , ['+']
+    ]
+  , concat [
+      replicate (addressWidth + 3) ' '
+      , '|' : "  0  1  2  3   4  5  6  7  8  9  a  b   c  d  e  f  "
+      , '|' : replicate (numLineBytes + 2) ' '
+    ]
+  , border
+  ]
+
 prettyHex :: ByteString -> String
-prettyHex bs = unlines (header : body)
+prettyHex bs = (unlines . concat) [ header, body, footer ]
  where
-  hexDisplayWidth = 50 -- Calculated width of the hex display panel
-  numLineWords    = 4  -- Number of words to group onto a line
-  addressWidth    = 4  -- Minimum width of a padded address
-  numLineBytes    = numLineWords * numWordBytes -- Number of bytes on a line
-  replacementChar = '.' -- 'Char' to use for non-printable characters
+  footer = [
+      border
+    , concat [ " Length: ", show (B.length bs), " (0x", showHex (B.length bs) ") bytes" ]
+    ]
 
-  header = "Length: " ++ show    (B.length bs)
-        ++ " (0x"     ++ showHex (B.length bs) ") bytes"
-
-  body = map (intercalate "   ")
+  body = map ((\x -> concat [ "| ", x, " |"]) . intercalate " | ")
        $ transpose [mkLineNumbers bs, mkHexDisplay bs, mkAsciiDump bs]
 
   mkHexDisplay
     = padLast hexDisplayWidth
     . map (intercalate "  ") . group numLineWords
-    . map (intercalate " ")  . group numWordBytes
+    . map unwords  . group numWordBytes
     . map (paddedShowHex byteWidth)
     . B.unpack
 
-  mkAsciiDump = group numLineBytes . cleanString . B8.unpack
+  mkAsciiDump = padLast numLineBytes . group numLineBytes . cleanString . B8.unpack
 
   cleanString = map go
    where
     go x | isWorthPrinting x = x
          | otherwise         = replacementChar
 
-  mkLineNumbers bs = [paddedShowHex addressWidth (x * numLineBytes) ++ ":"
+  mkLineNumbers bs = [paddedShowHex addressWidth (x * numLineBytes)
                      | x <- [0 .. (B.length bs - 1) `div` numLineBytes] ]
 
-  padLast w [x]         = [x ++ replicate (w - length x) ' ']
-  padLast w (x:xs)      = x : padLast w xs
-  padLast _ []          = []
+  padLast w [x]    = [x ++ replicate (w - length x) ' ']
+  padLast w (x:xs) = x : padLast w xs
+  padLast _ []     = []
 
 -- |'paddedShowHex' displays a number in hexidecimal and pads the number
 -- with 0 so that it has a minimum length of @w@.
